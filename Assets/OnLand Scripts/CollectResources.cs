6dd07622b63128c0654accd91100f46c0bbcf8f5
wastedtalents <kabadzhg@uni.coventry.ac.uk>
2016-03-06 23:43:54 +0000

    Only Raycast destruction done

---
+++ Assets/OnLand Scripts/CollectResources.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectResources : MonoBehaviour {
+//
+ //   GameObject canvas;
+	// Use this for initialization
+	void Start () {
+
+ //       canvas = GameObject.Find("Canvas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, out hit, 3f))
+        {
+            if (hit.collider.gameObject.tag.Equals("Rock"))
+            {
+                print(hit.collider.gameObject.name);
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/OnLand Scripts/ResourceCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceCounter : MonoBehaviour {
+
+    private GameObject[] resources;
+
+	// Use this for initialization
+	void Start () {
+
+        resources = new GameObject[transform.childCount];
+	
+        for (int i = 0; i <transform.childCount; i++)
+        {
+            resources[i] = transform.GetChild(i).gameObject;
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
